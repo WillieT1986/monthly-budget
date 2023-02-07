283f94a5a7c0cd18c8a5ba1dd0f2f6e12151bd72
WillieT1986 <wrthompsonjr86@gmail.com>
2023-02-07 15:27:25 -0500

    Through unit testing, the User Class was instantiated, Getters & Setters for username, first name, last name, email, and password, as well as tested for Valid User Creation, Given Invalid User Details When Creating User Throw Exception, Valid Username, Invalid Username, Invalid Username with Special Characters, and then finally tested for an Invalid Username that prevent SQL Injection.
    There were 4 warnings that were corrected:
    
    Warning 1: Condition 'email != null' is always 'true'
    
    This was corrected by simplifying return email != null && matcher.matches(); to just return matcher.matches(); since the warning is pointing out that the email variable is never null, so the check email != null is unnecessary.
    
    The other 3 warnings were pointing to three test cases: givenInvalidUserDetails_whenCreatingUser_thenThrowsException(), testInvalidUserNameSpecialChars(), and testInvalidUserNameSQLInjection() stating "Statement lambda can be replaced with expression lambda."
    
    These were corrected by turning their statements into expressions. Removed {} and an extra ;.

---
+++ src/main/java/model/user/User.java
@@ -0,0 +1,107 @@
+package model.user;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class User {
+
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    private static final int MIN_PASSWORD_LENGTH = 8;
+    String username;
+    String first_name;
+    String last_name;
+    String email;
+    String password;
+
+
+
+    public User(String username, String first_name, String last_name, String email, String password) {
+        if (!isValidUsername(username)) {
+            throw new IllegalArgumentException("Invalid username");
+        }
+        if (!isValidFirstName(first_name)) {
+            throw new IllegalArgumentException("Invalid first name");
+        }
+        if (!isValidLastName(last_name)) {
+            throw new IllegalArgumentException("Invalid last name");
+        }
+        if (!isValidEmail(email)) {
+            throw new IllegalArgumentException("Invalid email");
+        }
+        if (!isValidPassword(password)) {
+            throw new IllegalArgumentException("Invalid password");
+        }
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    private boolean isValidUsername(String username) {
+            return username != null && username.length() > 0;
+    }
+
+    private boolean isValidFirstName(String firstName) {
+        return firstName != null && firstName.length() > 0;
+    }
+
+    private boolean isValidLastName(String lastName) {
+        return lastName != null && lastName.length() > 0;
+    }
+
+    private boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(email);
+        return  matcher.matches();
+    }
+
+    private boolean isValidPassword(String password) {
+        return password != null && password.length() >= MIN_PASSWORD_LENGTH;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        if (!username.matches("^[a-zA-Z0-9]+$")) {
+            throw new IllegalArgumentException("Username can only contain letters and numbers");
+        }
+        this.username = username;
+    }
+
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
---
+++ src/test/java/model/user/UserTest.java
@@ -0,0 +1,193 @@
+package model.user;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+
+    private static final String USERNAME = "Will2023";
+    private static final String FIRST_NAME = "William";
+    private static final String LAST_NAME = "Thompson";
+    private static final String EMAIL = "fakeEmail@gmail.com";
+    private static final String PASSWORD = "123456789";
+
+    User underTest;
+
+    @BeforeEach
+    public void setUp() {
+        underTest = new User(USERNAME, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD);
+    }
+
+    //Test Cases for Instantiation of the User Class.
+    @Test
+    public void shouldInstantiateUserClass() {
+        assertNotNull(underTest);
+    }
+
+    //Test Cases for Getters & Setters
+    @Test
+    public void testGettersAndSetters() {
+        assertEquals("Will2023", underTest.getUsername());
+        assertEquals("William", underTest.getFirstName());
+        assertEquals("Thompson", underTest.getLastName());
+        assertEquals("fakeEmail@gmail.com", underTest.getEmail());
+        assertEquals("123456789", underTest.getPassword());
+
+        underTest.setUsername("NewUsername");
+        underTest.setFirstName("Jane");
+        underTest.setLastName("Doe");
+        underTest.setEmail("");
+        underTest.setPassword("newPassword");
+        assertEquals("NewUsername", underTest.getUsername());
+        assertEquals("Jane", underTest.getFirstName());
+        assertEquals("Doe", underTest.getLastName());
+        assertEquals("", underTest.getEmail());
+        assertEquals("newPassword", underTest.getPassword());
+    }
+
+    //Test Cases for User Creation
+    @Test
+    public void testValidUserCreation() {
+        assertNotNull(underTest);
+    }
+
+    @Test
+    public void givenInvalidUserDetails_whenCreatingUser_thenThrowsException() {
+       assertThrows(IllegalArgumentException.class, () ->
+               underTest = new User(USERNAME, FIRST_NAME, LAST_NAME, "invalid_email", PASSWORD)
+       );
+    }
+
+    //Test Cases for UserName
+    @Test
+    public void testValidUserName() {
+        underTest = new User("ValidUsername12", FIRST_NAME, LAST_NAME, EMAIL, PASSWORD);
+        underTest.setUsername("ValidUsername125");
+        assertEquals("ValidUsername125", underTest.getUsername());
+    }
+
+    @Test
+    public void testInvalidUserNameSpecialChars() {
+    //  If an error, specifically an "IllegalArgumentException", is not thrown, then the test case fails.
+        assertThrows(IllegalArgumentException.class, () ->
+                underTest.setUsername("invalidUsername@#$%^&*()_+")
+        );
+    }
+
+    @Test
+    public void testInvalidUserNameSQLInjection() {
+    //  If an error, specifically an "IllegalArgumentException", is not thrown, then the test case fails.
+        assertThrows(IllegalArgumentException.class, () ->
+                underTest.setUsername("invalidUsername'; DROP TABLE users;")
+        );
+    }
+
+
+    //Test Cases for First Name
+//    @Test
+//    public void testValidFirstName() {
+        // Todo: Implement test
+//    }
+
+//    @Test
+//    public void givenInvalidFirstName_whenSetFirstName_thenThrowException() {
+//        // Todo: Implement test
+//    }
+
+//    @Test(expected = ConstraintViolationException.class)
+//    public void whenFirstNameIsBlank_thenThrowException() {
+        // Todo: Implement test
+//    }
+
+
+
+
+    //Test Cases for Last Name
+//    @Test
+//    public void testValidLastName() {
+        // Todo: Implement test
+//    }
+
+//    @Test
+//    public void givenInvalidLastName_whenSetLastName_thenThrowException() {
+        // Todo: Implement test
+//    }
+
+//    @Test(expected = ConstraintViolationException.class)
+//    public void whenLastNameIsBlank_thenThrowException() {
+        // Todo: Implement test
+//    }
+
+
+
+
+
+
+    //Test Cases for Email
+//    @Test
+//    public void testValidEmail() {
+        // Todo: Implement test
+//    }
+
+//    @Test
+//    public void testInvalidEmail() {
+        // Todo: Implement test
+//    }
+
+//    @Test
+//    public void givenInvalidEmail_whenSettingEmail_thenThrowsException() {
+        // Todo: Implement test
+//    }
+
+
+
+
+    //Test Cases for Password
+//    @Test
+//    public void testValidPassword() {
+        // Todo: Implement test
+//    }
+
+//    @Test
+//    public void testInvalidPassword() {
+        // Todo: Implement test
+//    }
+
+//    @Test
+//    public void givenInvalidPassword_whenSettingPassword_thenThrowsException() {
+        // Todo: Implement test
+//    }
+
+
+
+
+
+
+    //Test Cases for Equality
+//    @Test
+//    public void testEquality() {
+        //Todo: Implement test
+//    }
+
+
+
+
+
+    //Test Cases for toString
+//    @Test
+//    public void testToString() {
+        // Todo: Implement test
+//    }
+
+
+
+
+    //Test Cases for HashCode
+//    @Test
+//    public void testHashCode() {
+        //Todo: Implement test
+//    }
+
+}
