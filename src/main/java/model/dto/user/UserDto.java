86f8c30a6bbaf385deabd109784affbf9907b670
WillieT1986 <wrthompsonjr86@gmail.com>
2023-02-24 17:47:14 -0500

    Through Unit Testing, created UserDto.java by testing UserDto, Equals, Hashcode, toString, toString Null Values (Which later became obsolete), Immutability, and Immutability Null Values. Also corrected a warning "Variable 'Other' can be replaced with pattern variable" that was inside UserDto.java. The change was replacing
    @Override
        public boolean equals(Object obj) {
            if (obj == this) {
                return true;
            }
            if (!(obj instanceof UserDto)) {
                return false;
            }
            UserDto other = (UserDto) obj;
            return Objects.equals(id, other.id)
                    && Objects.equals(username, other.username)
                    && Objects.equals(firstName, other.firstName)
                    && Objects.equals(lastName, other.lastName)
                    && Objects.equals(email, other.email);
        }
    
        with this new code
    
        @Override
        public boolean equals(Object obj) {
            if (obj == this) {
                return true;
            }
            if (!(obj instanceof UserDto)) {
                return false;
            }
            return Objects.equals(id, ((UserDto) obj).id)
                    && Objects.equals(username, ((UserDto) obj).username)
                    && Objects.equals(firstName, ((UserDto) obj).firstName)
                    && Objects.equals(lastName, ((UserDto) obj).lastName)
                    && Objects.equals(email, ((UserDto) obj).email);
        }

---
+++ src/main/java/model/dto/user/UserDto.java
@@ -0,0 +1,81 @@
+package model.dto.user;
+
+import model.user.User;
+
+import java.util.Objects;
+
+public final class UserDto {
+    private final Long id;
+    private final String username;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+
+    public UserDto(Long id, String username, String firstName, String lastName, String email) {
+        this.id = Objects.requireNonNull(id, "id must not be null");
+        this.username = Objects.requireNonNull(username, "username must not be null");
+        this.firstName = Objects.requireNonNull(firstName, "firstName must not be null");
+        this.lastName = Objects.requireNonNull(lastName, "lastName must not be null");
+        this.email = Objects.requireNonNull(email, "email must not be null");
+    }
+
+    public UserDto(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.email = user.getEmail();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof UserDto)) {
+            return false;
+        }
+        return Objects.equals(id, ((UserDto) obj).id)
+                && Objects.equals(username, ((UserDto) obj).username)
+                && Objects.equals(firstName, ((UserDto) obj).firstName)
+                && Objects.equals(lastName, ((UserDto) obj).lastName)
+                && Objects.equals(email, ((UserDto) obj).email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, firstName, lastName, email);
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
---
+++ src/test/java/model/dto/user/UserDtoTest.java
@@ -0,0 +1,100 @@
+package model.dto.user;
+
+import model.user.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserDtoTest {
+
+    @Test
+    public void testUserDTO() {
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("UsernameDto1");
+        user.setFirstName("FirstName");
+        user.setLastName("LastName");
+        user.setEmail("Email@Hotmail.com");
+
+        UserDto dto = new UserDto(user);
+
+        assertEquals(user.getId(), dto.getId());
+        assertEquals(user.getUsername(), dto.getUsername());
+        assertEquals(user.getFirstName(), dto.getFirstName());
+        assertEquals(user.getLastName(), dto.getLastName());
+        assertEquals(user.getEmail(), dto.getEmail());
+    }
+
+    @Test
+    public void testEquals() {
+        /* Testing if two instances of a class are the same is important because it helps ensure
+           that the object's identity is consistent and maintained throughout the code.
+           It is done using the assertEquals method to compare two instances and ensure that
+           they are equal in terms of their fields. */
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("UsernameDto2");
+        user.setFirstName("FirstName");
+        user.setLastName("LastName");
+        user.setEmail("EMAIL@Gmail.com");
+
+        UserDto dto = new UserDto(user);
+
+        assertEquals(dto, dto);
+        assertEquals(dto, new UserDto(user));
+    }
+
+    @Test
+    void testHashCode() {
+        UserDto userDto1 = new UserDto(1L, "UserName10", "John", "Doe", "fakeEmail@Gmail.com");
+        UserDto userDto2 = new UserDto(1L, "UserName10", "John", "Doe", "fakeEmail@Gmail.com");
+        UserDto userDto3 = new UserDto(2L, "UserName30", "Jane", "Doe", "fakeEmail2@Gmail.com");
+
+        assertEquals(userDto1.hashCode(), userDto2.hashCode());
+        assertNotEquals(userDto1.hashCode(), userDto3.hashCode());
+    }
+
+
+    @Test
+    public void testToString() {
+        UserDto dto = new UserDto(1L, "user1", "John", "Doe", "john.doe@example.com");
+
+        String expected = "UserDto{id=1, username='user1', firstName='John', lastName='Doe', email='john.doe@example.com'}";
+        assertEquals(expected, dto.toString());
+    }
+
+    @Test
+    public void testToString_NullValues() {
+        // With the addition of the UserDto constructor for testing Immutability,
+        // this test is no longer valid.
+
+        // UserDto dto = new UserDto(null, null, null, null, null);
+        // String expected = "UserDto{id=null, username='null', firstName='null', lastName='null', email='null'}";
+        // assertEquals(expected, dto.toString());
+    }
+
+    @Test
+    public void testImmutability() {
+        UserDto dto = new UserDto(1L, "user2", "John", "Doe", "john.doe@example.com");
+
+        assertEquals(1L, dto.getId());
+        assertEquals("user2", dto.getUsername());
+        assertEquals("John", dto.getFirstName());
+        assertEquals("Doe", dto.getLastName());
+        assertEquals("john.doe@example.com", dto.getEmail());
+    }
+
+    @Test
+    public void testImmutability_NullValues() {
+        // If the constructor of UserDto throws a NullPointerException, the test passes. Otherwise, the test fails.
+        // This approach ensures that a user cannot create a UserDto with a null value.
+        assertThrows(NullPointerException.class, () -> new UserDto(null, "username", "first", "last", "email"));
+        assertThrows(NullPointerException.class, () -> new UserDto(1L, null, "first", "last", "email"));
+        assertThrows(NullPointerException.class, () -> new UserDto(1L, "username", null, "last", "email"));
+        assertThrows(NullPointerException.class, () -> new UserDto(1L, "username", "first", null, "email"));
+        assertThrows(NullPointerException.class, () -> new UserDto(1L, "username", "first", "last", null));
+    }
+
+
+
+}
